f48741b85bdd9b23dda4f163c18126813b1a9e74
Кирилл Галимов <galimovdev@gmail.com>
2022-12-15 23:56:44 +0400

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import helmet from 'helmet';
+
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger/swagger.json' assert { type: 'json' };
 
@@ -12,6 +14,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 /**
  * Swagger
