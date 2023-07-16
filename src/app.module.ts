4db0473c375c3d67a9d3791c5bfa713eeb407c0a
Kirill Galimov <galimovdev@gmail.com>
2023-07-16 22:40:45 +0400

    feat: project init

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'src/app.module';
+
+const start = async () => {
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(PORT, () => {
+    console.log(`Server has been started on ${PORT} port`);
+  });
+};
+
+start();
