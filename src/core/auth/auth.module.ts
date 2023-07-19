6fd670c28925fd1f5e57b728820d8fd5e1fa4ab7
Kirill Galimov <galimovdev@gmail.com>
2023-07-20 01:12:42 +0400

    feat: auth guard

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/core/users/users.module';
+import { AuthGuard } from 'src/core/auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
   imports: [
     UsersModule,
